dd393f28a7e74e8ef2ca1a062cdaea214b90f828
Mayil Kerimov <mayilik@code.edu.az>
2022-06-05 18:32:36 +0400

    First Commit

---
+++ dist/js/cart.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(".cart-open").click(function(e){
+        e.preventDefault();
+        $("#shop-cart").css("transform","translateX(0)");
+        $(".cart-backdrop").addClass("is-visible");
+        $('body').css('overflow','hidden')
+    });
+    $("#close-cart").click(function(e){
+        e.preventDefault();
+        $("#shop-cart").css("transform","translateX(600px)");
+        $(".cart-backdrop").removeClass("is-visible");
+        $('body').css('overflow','auto')
+    })
+
+    $('#mobile-menu-open-close').click(function(){
+        $('.open-m, .close-m').toggleClass('d-none');
+        $('.open-drower-parent').toggleClass('mobile_isVisible');
+    })
+
+    $('.open_expand').click(function(){
+        $(this).parent().next().addClass('mobile-expand-visible');
+        $('.mobile-menu_secondary').toggleClass('d-none');
+    })
+
+    $('.close-expand').click(function(){
+        $(this).parent().parent().parent().removeClass("mobile-expand-visible");
+        $('.mobile-menu_secondary').toggleClass('d-none');
+    })
+
+    $('.rotate').click(function(){
+        $(this).toggleClass('plus-minus-rotate-closed')
+        $(this).toggleClass('plus-minus-rotate-open');
+    })
+});

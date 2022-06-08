7e46c5191c1b35ca192e3a83d5c12089337aa632
Mayil Kerimov <mayilik@code.edu.az>
2022-06-08 04:46:39 +0400

    Third commit

---
+++ dist/js/homeSlider.js
@@ -0,0 +1,13 @@
+$('.autoplay').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    dotsClass: "my-dots",
+    arrows: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next')
+  });
+
+  
